5807d4e6e95a4669a7c5e21c2cdba721bf7a26ee
dduvnjak <dario.duvnjak@gmail.com>
2016-06-15 12:36:13 +0200

    Squashed 'modules/dtk_postgresql/' changes from b037650..e77dc79
    e77dc79 k
    aed3cbf Initial commit.
    
    git-subtree-dir: modules/dtk_postgresql
    git-subtree-split: e77dc79349d1d5aaf808b925b98ab150e4d171d6

--- manifests/db.pp
+++ manifests/db.pp
@@ -5,7 +5,7 @@ define dtk_postgresql::db($db_name)
 
   $cmd = "createdb ${db_name} -U postgres ${hostname_argument}"
   $check_query = "select count(*) from pg_database where datname = '${db_name}';"
-  $check = inline_template('psql --tuples-only -U postgres ${hostname_argument} --command "<%= check_query %>" | grep 0') 
+  $check = inline_template('psql --tuples-only -U postgres ${hostname_argument} --command "<%= @check_query %>" | grep 0') 
   
   exec { "create-${db_name}" :
   	command => $cmd,
